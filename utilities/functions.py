a80421d6aeff93ae1bce67f6ff1c64294827b74d
David Machado <machadodavid32@gmail.com>
2023-11-02 09:12:19 -0300

    adicionando modulos

--- utilities/functions.py
+++ utilities/functions.py
@@ -8,3 +8,9 @@ def trocar_conta():
     pyautogui.click(1323,693, duration=2) # vai clicar mais abaixo para aumentar compatibilidade
     pyautogui.click(1245,627, duration=2)
     sleep(2)
+
+def clique_fora():
+    # Clique fora - eviar inf pra guardar nome de usuario pelo browser
+    pyautogui.click(1391,314, duration=2)
+    pyautogui.click(1388,371, duration=2)
+    sleep(2)
